3dcf7f5cd2112d382f04a8dd5a524abbc03506a9
Jelle Overbeek <j.overbeek@recognize.nl>
2018-06-19 23:56:10 +0200

    initial commit with npm scripts

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+  map: true,
+  plugins: {
+    "postcss-preset-env": {},
+    autoprefixer: {}
+  }
+};
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+// main.js
+const animation = require("./animation");
+const slider = require("./animation");
